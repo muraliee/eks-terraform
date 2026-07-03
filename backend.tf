dfbdcefd884d77a01e90eef1a96e3939ddc2436f
muraliee <muraliee.k@gmail.com>
2026-07-03 09:52:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket = "my-terraform-state-mohan-bucket" 
     key = "eks-cluster/terraform.tfstate" 
     region = "eu-north-1" 
-    dynamodb_table = "terraform-lock-table"
-     encrypt = true 
-     } 
+    
+        } 
     }
